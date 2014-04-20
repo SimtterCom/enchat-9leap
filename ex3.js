abd7d2bd0e21be5da0cb3ca029bc3e92c2eeebe3
OpenShift guest <j238b4pe@gmail.com>
2014-04-20 10:40:21 -0400

    change the project name

--- ex3.js
+++
@@ -1,8 +0,0 @@
-var app = require( 'express' ).createServer();
-
-app.get( '/', function( req, res ) {
-  res.send( 'Hello World!' );
-});
-app.listen(20005);
-
-console.log("Server started.");
--- ex4.js
+++
@@ -1,9 +0,0 @@
-var express = require( 'express' );
-
-var app = express.createServer();
-app.configure( function() {
-  app.use( express.static( __dirname + '/public' ) );
-});
-app.listen( 20005 );
-
-console.log( "Server started." );
